e3f474e80c6646acc91ad79a9ea667cc74021f32
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-09-16 14:00:11 +0300

    file_reader created

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,18 @@
+import pytest
+
 from src.classes import Product
 
-import pytest
 
 @pytest.fixture
 def product_1():
-    return Product('LG', '4К OLED-экран', 70000.0, 3)
+    return Product("LG", "4К OLED-экран", 70000.0, 3)
+
 
 @pytest.fixture
 def product_2():
-    return Product('Sony', 'HDR10+', 100000.0, 2)
+    return Product("Sony", "HDR10+", 100000.0, 2)
 
 
 @pytest.fixture
 def product_3():
-    return Product('Bosh', '10 различных программ', 20000.0, 4)
+    return Product("Bosh", "10 различных программ", 20000.0, 4)
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,20 +1,21 @@
-from src.classes import Product, Category
+from src.classes import Category, Product
+
 
 def test_create_product(product_1):
-    assert product_1.name == 'LG'
-    assert product_1.description == '4К OLED-экран'
+    assert product_1.name == "LG"
+    assert product_1.description == "4К OLED-экран"
     assert product_1.price == 70000.0
     assert product_1.quantity == 3
 
 
-def test_create_categories(product_1,product_2,product_3):
-    tv_products = Category('TV', 'Premium class', [product_1, product_2])
-    assert tv_products.name == 'TV'
-    assert tv_products.description == 'Premium class'
+def test_create_categories(product_1, product_2, product_3):
+    tv_products = Category("TV", "Premium class", [product_1, product_2])
+    assert tv_products.name == "TV"
+    assert tv_products.description == "Premium class"
     assert Category.product_count == 2
     assert Category.category_count == 1
-    washing_machines = Category('Washing machines', 'Очищает даже самые грязные пятна', [product_3])
-    assert washing_machines.name == 'Washing machines'
-    assert washing_machines.description == 'Очищает даже самые грязные пятна'
+    washing_machines = Category("Washing machines", "Очищает даже самые грязные пятна", [product_3])
+    assert washing_machines.name == "Washing machines"
+    assert washing_machines.description == "Очищает даже самые грязные пятна"
     assert Category.category_count == 2
     assert Category.product_count == 3
