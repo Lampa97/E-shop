e31cdbcc21a2af1aa90fb193831ececbfefb619d
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-09-18 13:14:48 +0300

    Added methods, getters and setters to classes

--- src/classes.py
+++ src/classes.py
@@ -7,8 +7,23 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def new_product(cls, params):
+        return cls(**params)
 
 
 class Category:
@@ -22,6 +37,16 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+
+    @property
+    def products(self):
+        product_list_string = ""
+        for product in self.__products:
+            product_list_string += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_list_string
