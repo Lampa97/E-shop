db1adc7d6fc24a076c8ab98b38358c349e3ab2ac
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-09-23 14:35:12 +0300

    additional tests done

---
+++ tests/test_classes_additional.py
@@ -0,0 +1,12 @@
+import pytest
+from src.classes import Category
+from src.classes_additional import CategoryIterator
+
+def test_category_iterator(product_1,product_2,product_3):
+    product_set = Category('Test', 'Test description', [product_1, product_2, product_3])
+    iterator = iter(CategoryIterator(product_set))
+    assert next(iterator) == "LG"
+    assert next(iterator) == "Sony"
+    assert next(iterator) == "Bosh"
+    with pytest.raises(StopIteration):
+        assert next(iterator)
