ccfd13dcbd0e35a01ef32f58a23663afa77f7d0c
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-09-23 14:04:08 +0300

    __add__ method for class Product

--- src/classes.py
+++ src/classes.py
@@ -13,6 +13,9 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
 
+    def __add__(self, other):
+        return self.quantity * self.__price + other.quantity * other.price
+
     @property
     def price(self):
         return self.__price
